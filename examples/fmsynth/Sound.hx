d6c510fce55ed42f05531a263d5a6838c85d78d1
Thomas J. Webb <thomas@osakared.io>
2018-05-09 21:10:39 -0700

    8 note polyphonic fm synth working in the browser!!

--- examples/fmsynth/Sound.hx
+++ examples/fmsynth/Sound.hx
@@ -24,14 +24,14 @@ class SynthChannel extends hxd.snd.NativeChannel {
 		}
 	}
 
-	public function noteOn()
+	public function noteOn(note:Int)
 	{
-		synth.noteOn(30, 64);
+		synth.noteOn(note, 64);
 	}
 
-	public function noteOff()
+	public function noteOff(note:Int)
 	{
-		synth.noteOff(30);
+		synth.noteOff(note);
 	}
 
 }
@@ -48,15 +48,64 @@ class Sound extends hxd.App {
 
 	override function update(dt:Float) {
 		if( hxd.Key.isPressed(hxd.Key.SPACE) ) {
-			trace('space is the place');
-			if (playing) {
-				playing = false;
-				synthChannel.noteOff();
-			}
-			else {
-				playing = true;
-				synthChannel.noteOn();
-			}
+			synthChannel.noteOn(18);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.SPACE) ) {
+			synthChannel.noteOff(18);
+		}
+		if( hxd.Key.isPressed(hxd.Key.A) ) {
+			synthChannel.noteOn(36);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.A) ) {
+			synthChannel.noteOff(36);
+		}
+		if( hxd.Key.isPressed(hxd.Key.S) ) {
+			synthChannel.noteOn(38);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.S) ) {
+			synthChannel.noteOff(38);
+		}
+		if( hxd.Key.isPressed(hxd.Key.D) ) {
+			synthChannel.noteOn(39);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.D) ) {
+			synthChannel.noteOff(39);
+		}
+		if( hxd.Key.isPressed(hxd.Key.F) ) {
+			synthChannel.noteOn(41);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.F) ) {
+			synthChannel.noteOff(41);
+		}
+		if( hxd.Key.isPressed(hxd.Key.G) ) {
+			synthChannel.noteOn(43);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.G) ) {
+			synthChannel.noteOff(43);
+		}
+		if( hxd.Key.isPressed(hxd.Key.H) ) {
+			synthChannel.noteOn(44);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.H) ) {
+			synthChannel.noteOff(44);
+		}
+		if( hxd.Key.isPressed(hxd.Key.J) ) {
+			synthChannel.noteOn(46);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.J) ) {
+			synthChannel.noteOff(46);
+		}
+		if( hxd.Key.isPressed(hxd.Key.K) ) {
+			synthChannel.noteOn(48);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.K) ) {
+			synthChannel.noteOff(48);
+		}
+		if( hxd.Key.isPressed(hxd.Key.L) ) {
+			synthChannel.noteOn(50);
+		}
+		else if( hxd.Key.isReleased(hxd.Key.L) ) {
+			synthChannel.noteOff(50);
 		}
 	}
 
